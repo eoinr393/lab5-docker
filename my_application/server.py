904a0c08613bd12e99597a621a47e8101e29a85a
eoinr393 <eoinraeside393@gmail.com>
2015-10-14 16:56:05 +0000

    this commit

--- my_application/server.py
+++ my_application/server.py
@@ -2,8 +2,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+	return "Index"
+
+@app.route("/hello")
 def hello():
     return "Hello World!"
 
+@app.route("/user/<username>")
+def user(username):
+	return "User %s" % username
+
+@app.route("/post/80")
+def post():
+	return "80"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",debug=True)
